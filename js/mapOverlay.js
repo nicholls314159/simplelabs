91b3cdd30ed0de12c39a9fd123effd60497933dd
nicholls314159 <nicholls3.14159@gmail.com>
2016-05-31 18:26:42 -0700

    Update mapOverlay.js

--- js/mapOverlay.js
+++ js/mapOverlay.js
@@ -1,15 +1,15 @@
 /**
-     * Create a custom overlay for our window marker display, extending google.maps.OverlayView.
-     * This is somewhat complicated by needing to async load the google.maps api first - thus, we
-     * wrap CustomWindow into a closure, and when instantiating CustomWindow, we first execute the closure (to create
-     * our CustomWindow function, now properly extending the newly loaded google.maps.OverlayView), and then
-     * instantiate said function.
-     * Note that this version uses jQuery.
-     * @type {Function}
-     */
-(function(){
+ * Create a custom overlay for our window marker display, extending google.maps.OverlayView.
+ * This is somewhat complicated by needing to async load the google.maps api first - thus, we
+ * wrap CustomWindow into a closure, and when instantiating CustomWindow, we first execute the closure
+ * (to create our CustomWindow function, now properly extending the newly loaded google.maps.OverlayView),
+ * and then instantiate said function.
+ * @type {Function}
+ */
+function GenCustomWindow(){
     var CustomWindow = function(){
-        this.container = $('<div class="map-info-window"></div>');
+        this.container = document.createElement('div');
+        this.container.classList.add('map-info-window');
         this.layer = null;
         this.marker = null;
         this.position = null;
@@ -25,12 +25,14 @@
      * @see CustomWindow.open
      */
     CustomWindow.prototype.onAdd = function(){
-        this.layer = $(this.getPanes().floatPane);
-        this.layer.append(this.container);
-        this.container.find('.map-info-close').on('click', _.bind(function(){
+        this.layer = this.getPanes().floatPane;
+        this.layer.appendChild(this.container);
+        this.container.getElementsByClassName('map-info-close')[0].addEventListener('click', function(){
             // Close info window on click
             this.close();
-        }, this));
+        }.bind(this), false);
+        // Ensure newly opened window is fully in view
+        setTimeout(this.panToView.bind(this), 200);
     };
     /**
      * Called after onAdd, and every time the map is moved, zoomed, or anything else that
@@ -38,27 +40,77 @@
      */
     CustomWindow.prototype.draw = function(){
         var markerIcon = this.marker.getIcon(),
-            cHeight = this.container.outerHeight() + markerIcon.scaledSize.height + 10,
-            cWidth = this.container.width() / 2 + markerIcon.scaledSize.width / 2;
+            cHeight = this.container.offsetHeight + markerIcon.scaledSize.height + 10,
+            cWidth = this.container.offsetWidth / 2;
         this.position = this.getProjection().fromLatLngToDivPixel(this.marker.getPosition());
-        this.container.css({
-            'top':this.position.y - cHeight,
-            'left':this.position.x - cWidth
-        });
+        this.container.style.top = this.position.y - cHeight+'px';
+        this.container.style.left = this.position.x - cWidth+'px';
+    };
+    /**
+     * If the custom window is not already entirely within the map view, pan the map the minimum amount
+     * necessary to bring the custom info window fully into view.
+     */
+    CustomWindow.prototype.panToView = function(){
+        var position = this.position,
+            latlng = this.marker.getPosition(),
+            top = parseInt(this.container.style.top, 10),
+            cHeight = position.y - top,
+            cWidth = this.container.offsetWidth / 2,
+            map = this.getMap(),
+            center = map.getCenter(),
+            bounds = map.getBounds(),
+            degPerPixel = (function(){
+                var degs = {},
+                    div = map.getDiv(),
+                    span = bounds.toSpan();
+
+                degs.x = span.lng() / div.offsetWidth;
+                degs.y = span.lat() / div.offsetHeight;
+                return degs;
+            })(),
+            infoBounds = (function(){
+                var infoBounds = {};
+
+                infoBounds.north = latlng.lat() + cHeight * degPerPixel.y;
+                infoBounds.south = latlng.lat();
+                infoBounds.west = latlng.lng() - cWidth * degPerPixel.x;
+                infoBounds.east = latlng.lng() + cWidth * degPerPixel.x;
+                return infoBounds;
+            })(),
+            newCenter = (function(){
+                var ne = bounds.getNorthEast(),
+                    sw = bounds.getSouthWest(),
+                    north = ne.lat(),
+                    east = ne.lng(),
+                    south = sw.lat(),
+                    west = sw.lng(),
+                    x = center.lng(),
+                    y = center.lat(),
+                    shiftLng = ((infoBounds.west < west) ? west - infoBounds.west : 0) +
+                        ((infoBounds.east > east) ? east - infoBounds.east : 0),
+                    shiftLat = ((infoBounds.north > north) ? north - infoBounds.north : 0) +
+                        ((infoBounds.south < south) ? south - infoBounds.south : 0);
+
+                return (shiftLng || shiftLat) ? new google.maps.LatLng(y - shiftLat, x - shiftLng) : void 0;
+            })();
+
+        if (newCenter){
+            map.panTo(newCenter);
+        }
     };
     /**
      * Called when this overlay has its map set to null.
      * @see CustomWindow.close
      */
     CustomWindow.prototype.onRemove = function(){
-        this.container.remove();
+        this.layer.removeChild(this.container);
     };
     /**
      * Sets the contents of this overlay.
      * @param {string} html
      */
     CustomWindow.prototype.setContent = function(html){
-        this.container.html(html);
+        this.container.innerHTML = html;
     };
     /**
      * Sets the map and relevant marker for this overlay.
@@ -76,6 +128,4 @@
         this.setMap(null);
     };
     return CustomWindow;
-});
-           
-Write 
+}
